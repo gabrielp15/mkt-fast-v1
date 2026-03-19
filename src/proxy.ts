02e66579d03afc1f51093757af583ce5607da4f0
Gabriel Pereira <gabriel.santos150598@gmail.com>
2026-03-19 18:11:52 -0300

    Hot fix: removido autenticação obrigatória

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,31 +10,7 @@ export function proxy(req: NextRequest) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  // Pegue o cabeçalho de autorização da requisição
-  const basicAuth = req.headers.get('authorization');
-
-  // Defina o seu usuário e senha aqui (O ideal é usar variáveis de ambiente)
-  const USERNAME = process.env.BASIC_AUTH_USER;
-  const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1];
-    const [user, pwd] = atob(authValue).split(':');
-
-    // Se o usuário e a senha estiverem corretos, permita o acesso
-    if (user === USERNAME && pwd === PASSWORD) {
-      return NextResponse.next();
-    }
-  }
-
-  // Se não houver autenticação ou se as credenciais estiverem erradas, peça a senha
-  url.pathname = '/api/auth';
-  return new NextResponse('Autenticação necessária', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  });
+  return NextResponse.next();
 }
 
 // Defina em quais rotas o middleware deve rodar
